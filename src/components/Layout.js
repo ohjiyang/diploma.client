d48d50cb12cd1ba3b0f94bfdb2e1a1b07b2dff3e
ohjiyang <ohjiyung@icloud.com>
2023-05-19 01:52:54 +0600

    style(Layout): initial styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
+import {RiMenuFill} from "react-icons/ri";
 
 const Layout = () => {
     const [isAuth, setIsAuth] = useState(false)
@@ -11,13 +12,44 @@ const Layout = () => {
         setIsAuth(false)
     }
 
+    const [isMenuActive, setIsMenuActive] = useState(false)
+
+    const handleOnToggleMenu = () => {
+        setIsMenuActive(prevState => !prevState)
+    }
+
     if (isAuth) {
         return (
             <div className={"wrapper"}>
-                <div>
-                    <button onClick={handleOnLogout}>Logout</button>
-                    Layout
-                </div>
+                <header className={"header"}>
+                    <div className={"header__container __container"}>
+                        <div className={"header__body"}>
+                            <div className={"header__navigation"}>
+                                <button className={"header__menu"} onClick={handleOnToggleMenu}>
+                                    <RiMenuFill size={35}/>
+                                </button>
+                            </div>
+                            <div className={"header__main"}>
+                                <button className={"header__logout"} onClick={handleOnLogout}>
+                                    Выйти
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <main className={"page"}>
+                    <div className={"page__container __container"}>
+                        <div className={"page__body"}>
+                            <div className={isMenuActive ? "page__navigation-active" : "page__navigation"}>
+                                page__navigation
+                            </div>
+                            <div className={"page__main"}>
+                                page__main
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
         );
     } else {
