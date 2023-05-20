f2bb459c35a3254e82221878e51c848ddf96d8e4
ohjiyang <ohjiyung@icloud.com>
2023-05-20 23:06:12 +0600

    feat(Navigation): added Modal windows

--- src/UI/ProjectContextMenu.js
+++ src/UI/ProjectContextMenu.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import {RiEditBoxLine, RiDeleteBin7Line, RiFileTransferLine} from "react-icons/ri";
 
-const ProjectContextMenu = ({pageX, pageY, onCloseContextMenu})=> {
+const ProjectContextMenu = ({pageX, pageY, onCloseContextMenu, onShareProject, onEditProject, onDeleteProject})=> {
     const useOnClickOutside = (ref, handler) => {
         useEffect(
             () => {
@@ -25,19 +25,34 @@ const ProjectContextMenu = ({pageX, pageY, onCloseContextMenu})=> {
     const contextMenuRef = useRef()
     useOnClickOutside(contextMenuRef, onCloseContextMenu)
 
+    const handleOnEditProject = () => {
+        onEditProject()
+        onCloseContextMenu()
+    }
+
+    const handleOnDeleteProject = () => {
+        onDeleteProject()
+        onCloseContextMenu()
+    }
+
+    const handleOnShareProject = () => {
+        onShareProject()
+        onCloseContextMenu()
+    }
+
     return (
         <div>
             <div className={"projectContextMenu"} style={{top: `${pageY}px`, left: `${pageX}px`}} ref={contextMenuRef}>
                 <div className={"projectContextMenu__body"}>
-                    <button className={"projectContextMenu__button"}>
+                    <button className={"projectContextMenu__button"} onClick={handleOnEditProject}>
                         Поменять название
                         <RiEditBoxLine size={20}/>
                     </button>
-                    <button className={"projectContextMenu__button"}>
+                    <button className={"projectContextMenu__button"} onClick={handleOnShareProject}>
                         Поделиться
                         <RiFileTransferLine size={20}/>
                     </button>
-                    <button className={"projectContextMenu__button"}>
+                    <button className={"projectContextMenu__button"} onClick={handleOnDeleteProject}>
                         Удалить
                         <RiDeleteBin7Line size={20}/>
                     </button>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {RiFileAddLine, RiFileSearchLine} from "react-icons/ri";
 import axios from "axios";
 import ProjectContextMenu from "../UI/ProjectContextMenu";
+import ProjectModalCreate from "../UI/ProjectModalCreate";
+import ProjectModalAdd from "../UI/ProjectModalAdd";
+import ProjectModalEdit from "../UI/ProjectModalEdit";
+import ProjectModalDelete from "../UI/ProjectModalDelete";
 
 const Navigation = ({isAuth, isMenuActive}) => {
     const [projects, setProjects] = useState([])
@@ -45,24 +49,93 @@ const Navigation = ({isAuth, isMenuActive}) => {
     const handleCloseContextMenu = () => {
         setContextMenu({show: false, x: 0, y: 0})
     }
+    //Модальные окна
+    const [isModalAddActive, setIsModalAddActive] = useState(false)
+    const [isModalCreateActive, setIsModalCreateActive] = useState(false)
+    const [isModalEditActive, setIsModalEditActive] = useState(false)
+    const [isModalDeleteActive, setIsModalDeleteActive] = useState(false)
+
+    const handleOnAddProject = () => {
+        setIsModalAddActive(true)
+    }
+
+    const handleOnCreateProject = () => {
+        setIsModalCreateActive(true)
+    }
+
+    const handleOnEditProject = () => {
+        setIsModalEditActive(true)
+    }
+
+    const handleOnShareProject = () => {
+        navigator.clipboard.writeText(project._id)
+    }
+
+    const handleOnDeleteProject = () => {
+        setIsModalDeleteActive(true)
+    }
 
     return (
         <div className={isMenuActive ? "navigation-active" : "navigation"}>
             <>
                 {
+                    isModalAddActive && <ProjectModalAdd isActive={isModalAddActive}
+                                                         onSetActive={setIsModalAddActive}
+                                                         isAuth={isAuth}
+                                                         onFetchProjects={handleOnFetchProjects}
+
+                    />
+                }
+            </>
+            <>
+                {
+                    isModalCreateActive && <ProjectModalCreate isActive={isModalCreateActive}
+                                                               onSetActive={setIsModalCreateActive}
+                                                               isAuth={isAuth}
+                                                               onFetchProjects={handleOnFetchProjects}
+
+                    />
+                }
+            </>
+            <>
+                {
+                    isModalEditActive && <ProjectModalEdit isActive={isModalEditActive}
+                                                           onSetActive={setIsModalEditActive}
+                                                           project={project}
+                                                           isAuth={isAuth}
+                                                           onFetchProjects={handleOnFetchProjects}
+                    />
+                }
+            </>
+            <>
+                {
+                    isModalDeleteActive && <ProjectModalDelete isActive={isModalDeleteActive}
+                                                               onSetActive={setIsModalDeleteActive}
+                                                               project={project}
+                                                               isAuth={isAuth}
+                                                               onFetchProjects={handleOnFetchProjects}
+
+                    />
+                }
+            </>
+            <>
+                {
                     contextMenu.show && <ProjectContextMenu pageX={contextMenu.x}
                                                             pageY={contextMenu.y}
                                                             onCloseContextMenu={handleCloseContextMenu}
+                                                            onEditProject={handleOnEditProject}
+                                                            onDeleteProject={handleOnDeleteProject}
+                                                            onShareProject={handleOnShareProject}
                     />
                 }
             </>
             <div className={"navigation__head"}>
-                <button className={"navigation__button"}>
+                <button className={"navigation__button"} onClick={handleOnAddProject}>
                     Получить проект
                     <RiFileAddLine size={20}/>
                 </button>
 
-                <button className={"navigation__button"}>
+                <button className={"navigation__button"} onClick={handleOnCreateProject}>
                     Создать проект
                     <RiFileSearchLine size={20}/>
                 </button>
