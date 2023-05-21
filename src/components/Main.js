f4ce91b88c5498b7f0c0fe62891de717093b9d1b
ohjiyang <ohjiyung@icloud.com>
2023-05-21 22:38:34 +0600

    style(Main): initial styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import {NavLink, Outlet} from "react-router-dom";
+import {RiCloseLine} from "react-icons/ri";
 
-const Main = () => {
+const Main = ({currentProject, onLoadProject}) => {
+    const handleOnCloseProject = () => {
+        onLoadProject({})
+    }
+
     return (
-        <div>
-            Main
+        <div className={"main"}>
+            <div className={"main__head"}>
+                <div className={"main__title"}>
+                    {
+                        currentProject.title
+                    }
+                </div>
+                <div className={"main__buttons"}>
+                    <button className={"main__button"} onClick={handleOnCloseProject}>
+                        <RiCloseLine size={35}/>
+                    </button>
+                </div>
+            </div>
+            <div className={"main__navlinks"}>
+                <NavLink to={"/"}
+                         className={({isActive}) => isActive ? "main__navlink-active" : "main__navlink"}>Обзор</NavLink>
+                <NavLink to={"/board"}
+                         className={({isActive}) => isActive ? "main__navlink-active" : "main__navlink"}>Доска</NavLink>
+                <NavLink to={"/monitoring"}
+                         className={({isActive}) => isActive ? "main__navlink-active" : "main__navlink"}>Мониторинг</NavLink>
+            </div>
+            <div className={"main__body"}>
+                <Outlet/>
+            </div>
         </div>
     );
 };
